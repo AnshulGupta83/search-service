0cb6b3eba58cba001afed05f29a82f439cad7afb
AnshulGupta83 <amshulgupta83@gmail.com>
2016-03-21 03:25:36 +1100

    Initial Check-in

---
+++ src/main/java/com/anshulgupta/assignment/json/SearchText.java
@@ -0,0 +1,18 @@
+package com.anshulgupta.assignment.json;
+
+import java.util.List;
+
+
+public class SearchText {
+	
+	private String[] searchText;
+
+	public String[] getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String[] searchText) {
+		this.searchText = searchText;
+	}
+		
+}
